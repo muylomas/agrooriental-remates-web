f3801c1151107f4382901fec9fb4dc8cc0cc500f
Martin A. López Espiña <martin@muylomas.com>
2025-11-22 23:17:22 -0300

    modified:   routes/aws_sms_sender.js

--- routes/aws_sms_sender.js
+++ routes/aws_sms_sender.js
@@ -8,32 +8,18 @@ const awsConfigEnv = fromIni({
 });
 
 Common.prototype.sms_sender = function (params, callback) {
-    console.log("================ sms_sender ================");
     const snsClient = new SNSClient({ region: "us-west-1", credentials: awsConfigEnv });
 
     snsClient.send(
         new PublishCommand(params),
     ).then(
         (response) => {
-            console.log(response);
             callback();
         },
         (error) => {
             console.log(error);
         }
     );
-
-    // {
-    //   '$metadata': {
-    //     httpStatusCode: 200,
-    //     requestId: '7410094f-efc7-5f52-af03-54737569ab77',
-    //     extendedRequestId: undefined,
-    //     cfId: undefined,
-    //     attempts: 1,
-    //     totalRetryDelay: 0
-    //   },
-    //   MessageId: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
-    // }
 };
 
 module.exports = new Common();
