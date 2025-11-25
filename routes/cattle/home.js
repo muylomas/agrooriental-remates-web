3b99622e5f58aebbfd4b9300fff9e725278e7dc6
Martin A. López Espiña <martin@muylomas.com>
2025-11-25 10:30:42 -0300

    modified:   routes/cattle/home.js

--- routes/cattle/home.js
+++ routes/cattle/home.js
@@ -252,8 +252,6 @@ Common.prototype.getViewParams = function (user, callback) {
                     indexParams.activeAuctionBids = [];
                     indexParams.cattleCaracteristics = common_cattle_home.cattleCaracteristics();
 
-                    console.log(indexParams.lots);
-
                     common_customers.getCustomerById(
                         user.id,
                         function (returnCustomer) {
