afbb52c8ee18857d860a1afbb0dcfa54480bc89b
Martin A. López Espiña <martin@muylomas.com>
2025-11-25 13:56:49 -0300

    modified:   public/remate/scripts.js

--- public/remate/scripts.js
+++ public/remate/scripts.js
@@ -3,10 +3,12 @@ function changeVideoImageDisplay(lotId) {
         !($("#image-" + lotId).is(":hidden")) ||
         $("#video-" + lotId).is(":hidden")
     ) {
+        console.log("Caso 1");
         $("#image-" + lotId).hide();
         $("#video-" + lotId).show();
     }
     else {
+        console.log("Caso 2");
         $("#image-" + lotId).show();
         $("#video-" + lotId).hide();
     }
