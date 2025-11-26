118b32d922e5620dd7b0bef295819d696473fc68
Martin A. López Espiña <martin@muylomas.com>
2025-11-25 23:38:47 -0300

    modified:   public/remate/slide-template.js

--- public/remate/slide-template.js
+++ public/remate/slide-template.js
@@ -19,53 +19,51 @@ const slideTemplate =
                     </div>
                 </div>
                 <div class="card-body px-3">
-                    <div class="row">
-                        <div class="col-2">
+                    <div class="d-flex justify-content-between flex-wrap">
+                        <div class="d-flex align-items-end flex-wrap">
                             <div class="btn btn-dark text-uppercase fs-5 p-2 m-0">
                                 <b>Lote __lot_lotId__</b>
                             </div>
                         </div>
-                        <div class="col-10">
-                            <div class="countdown-container d-flex flex-wrap justify-content-xl-between" id="bottom-countdown-container-__lot_lotId__">
-                                <div class="d-flex flex-grow-1 align-items-left justify-content-left justify-content-md-left">
-                                    <div class="countdown-day-container d-flex flex-column justify-content-around">
-                                        <h2 class="mb-0 d-inline-block text-center p-2">
-                                            <span class="text-white" id="countdown-days-__lot_lotId__">
-                                                --
-                                            </span>
-                                        </h2>
-                                    </div>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <h2 class="mb-0 d-inline-block text-center p-2">
-                                            <span class="text-white" id="countdown-hours-__lot_lotId__">
-                                                --
-                                            </span>
-                                        </h2>
-                                    </div>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <h2 class="mb-0 d-inline-block text-center p-2">
-                                            <span class="text-white">:</span>
-                                        </h2>
-                                    </div>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <h2 class="mb-0 d-inline-block text-center p-2">
-                                            <span class="text-white" id="countdown-mins-__lot_lotId__">
-                                                --
-                                            </span>
-                                        </h2>
-                                    </div>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <h2 class="mb-0 d-inline-block text-center p-2">
-                                            <span class="text-white">:</span>
-                                        </h2>
-                                    </div>
-                                    <div class="d-flex flex-column justify-content-around">
-                                        <h2 class="mb-0 d-inline-block text-center p-2">
-                                            <span class="text-white" id="countdown-secs-__lot_lotId__">
-                                                --
-                                            </span>
-                                        </h2>
-                                    </div>
+                        <div class="countdown-container d-flex justify-content-between align-items-end flex-wrap" id="bottom-countdown-container-__lot_lotId__">
+                            <div class="d-flex flex-grow-1 align-items-left justify-content-left justify-content-md-left">
+                                <div class="countdown-day-container d-flex flex-column justify-content-around">
+                                    <h2 class="mb-0 d-inline-block text-center p-2">
+                                        <span class="text-white" id="countdown-days-__lot_lotId__">
+                                            --
+                                        </span>
+                                    </h2>
+                                </div>
+                                <div class="d-flex flex-column justify-content-around">
+                                    <h2 class="mb-0 d-inline-block text-center p-2">
+                                        <span class="text-white" id="countdown-hours-__lot_lotId__">
+                                            --
+                                        </span>
+                                    </h2>
+                                </div>
+                                <div class="d-flex flex-column justify-content-around">
+                                    <h2 class="mb-0 d-inline-block text-center p-2">
+                                        <span class="text-white">:</span>
+                                    </h2>
+                                </div>
+                                <div class="d-flex flex-column justify-content-around">
+                                    <h2 class="mb-0 d-inline-block text-center p-2">
+                                        <span class="text-white" id="countdown-mins-__lot_lotId__">
+                                            --
+                                        </span>
+                                    </h2>
+                                </div>
+                                <div class="d-flex flex-column justify-content-around">
+                                    <h2 class="mb-0 d-inline-block text-center p-2">
+                                        <span class="text-white">:</span>
+                                    </h2>
+                                </div>
+                                <div class="d-flex flex-column justify-content-around">
+                                    <h2 class="mb-0 d-inline-block text-center p-2">
+                                        <span class="text-white" id="countdown-secs-__lot_lotId__">
+                                            --
+                                        </span>
+                                    </h2>
                                 </div>
                             </div>
                         </div>
