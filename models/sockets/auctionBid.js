ea495880ab4d17b529ca8749d90efaa09ff4a98e
Martin A. López Espiña <martin@muylomas.com>
2025-12-02 20:25:42 -0300

    modified:   models/sockets/auctionBid.js

--- models/sockets/auctionBid.js
+++ models/sockets/auctionBid.js
@@ -2,6 +2,9 @@
  * Serve content over a socket
  */
 const connection = require('../../routes/connection_db');
+const aws_sms_sender = require('../../routes/aws_sms_sender');
+
+
 
 function getUserBySocket(socketId, callback) {
     connection.query(
@@ -28,14 +31,20 @@ function getUserBySocket(socketId, callback) {
 }
 
 function isBidOk(bid, lotId, callback) {
-    let reply = { error: true, msg: "Pique no válido" };
+    let reply = { error: true, msg: "Pique no válido", customer: {} };
     connection.query(
         `
             SELECT
-                startPrice, 
-                stepPrice
+                cattle.startPrice AS startPrice, 
+                cattle.stepPrice AS stepPrice,
+                customers_complete.customerId AS customerId,
+                customers_complete.phoneCountryCode AS phoneCountryCode,
+                customers_complete.phoneNumber AS phoneNumber
             FROM cattle
-            WHERE id = ?
+            LEFT JOIN auctions_bids_max ON auctions_bids_max.lotId = cattle.id
+            LEFT JOIN auctions_bids ON auctions_bids.id = auctions_bids_max.auctionBidId
+            LEFT JOIN customers_complete ON customers_complete.customerId = auctions_bids.customerId
+            WHERE cattle.id = ?
         `,
         [
             lotId,
@@ -45,14 +54,25 @@ function isBidOk(bid, lotId, callback) {
                 console.log(err);
             }
 
-            if (bid < results[0].startPrice) {
-                reply.msg = "El pique debe superar la base de USD " + results[0].startPrice + ". Vuelva a intenarlo!";
+            if (results.length) {
+                if (bid < results[0].startPrice) {
+                    reply.msg = "El pique debe superar la base de USD " + results[0].startPrice + ". Vuelva a intenarlo!";
+                }
+                else if ((bid - results[0].startPrice) % results[0].stepPrice === 0) {
+                    reply.msg = " ";
+                    reply.error = false;
+                } else {
+                    reply.msg = "El pique debe respetar los incrementos de USD " + results[0].stepPrice + ". Vuelva a intenarlo!";
+                }
+
+                reply.customer = {
+                    customerId: results[0].customerId,
+                    phoneCountryCode: results[0].phoneCountryCode,
+                    phoneNumber: results[0].phoneNumber,
+                }
             }
-            else if ((bid - results[0].startPrice) % results[0].stepPrice === 0) {
-                reply.msg = " ";
-                reply.error = false;
-            } else {
-                reply.msg = "El pique debe respetar los incrementos de USD " + results[0].stepPrice + ". Vuelva a intenarlo!";
+            else {
+                reply.msg = "Problemas para registrar la oferta. Contacte con Agro Oriental al +598 98 950 792 o uelva a intenarlo!";
             }
 
             callback(reply);
@@ -62,27 +82,23 @@ function isBidOk(bid, lotId, callback) {
 
 function isSalePrice(lastBid, salePrice, lotId, callback) {
     if (salePrice <= lastBid) {
-        /*connection.query(
-            `
-                UPDATE cattle SET
-                auctionEnd = NOW() - INTERVAL 3 HOUR
-                WHERE id = ?
-            `,
-            [
-                lotId,
-            ],
-            function (err, results) {
-                if (err) {
-                    console.log(err);
-                }
-                callback(true);
-            }
-        );*/
         callback(true);
     }
     else
         callback(false);
-}
+};
+
+function sendSMSToLooser(customer, lotId) {
+    aws_sms_sender.sms_sender(
+        {
+            Message: "Superaron tu oferta para el lote " + lotId + ". Superala si no querés perderlo https://equinos.agrooriental.uy/#lote-" + lotId,
+            PhoneNumber: customer.phoneCountryCode + parseInt(customer.phoneNumber, 10),
+        },
+        function (error) {
+        }
+    );
+};
+
 
 module.exports = function (socket) {
     socket.on('auctionBidCustomers', (parameters) => {
@@ -98,6 +114,7 @@ module.exports = function (socket) {
                             parameters.bid,
                             parameters.lotId,
                             function (bidCheckReply) {
+                                const lastAuctionCustomer = bidCheckReply.customer;
                                 if (!bidCheckReply.error) {
                                     connection.query(
                                         `
@@ -186,6 +203,10 @@ module.exports = function (socket) {
                                                                         'auctionBidUpdate',
                                                                         __aux_newBidMsg,
                                                                     );
+
+                                                                    if (lastAuctionCustomer.customerId != customerId) {
+                                                                        sendSMSToLooser(lastAuctionCustomer);
+                                                                    }
                                                                 }
                                                             );
                                                         }
