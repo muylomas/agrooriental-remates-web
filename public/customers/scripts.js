bc5ea8cfb9667400cdc8db467d620a1d51f145e0
Martin A. López Espiña <martin@muylomas.com>
2025-12-10 14:13:28 -0300

    modified:   public/customers/scripts.js 	modified:   views/customers/profile.pug

--- public/customers/scripts.js
+++ public/customers/scripts.js
@@ -2,3 +2,12 @@ function goBackwards() {
     window.history.go(-1);
     return false;
 };
+
+function updateOnNidType() {
+    if ($("#nid-type-id").val() == 1) {
+        $("#company-name-container").addClass("d-none");
+    }
+    else {
+        $("#company-name-container").removeClass("d-none");
+    }
+};
