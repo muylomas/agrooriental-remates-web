f95e5c4ddc339192e0129a98204267bc914e893d
Martin A. López Espiña <martin@muylomas.com>
2025-12-11 08:55:45 -0300

    modified:   public/customers/scripts.js

--- public/customers/scripts.js
+++ public/customers/scripts.js
@@ -6,6 +6,12 @@ function goBackwards() {
 function updateOnNidType() {
     if ($("#nid-type-id").val() == 1) {
         $("#label-company-name").html("Nombre");
+        if (!$("input[name='companyName']").val()) {
+            $("input[name='companyName']").val(
+                $("input[name='name']").val() + " " +
+                $("input[name='surname']").val()
+            )
+        }
     }
     else {
         $("#label-company-name").html("Razón social");
