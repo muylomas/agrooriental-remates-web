c84a45791f431d82e0d377886aee19eb5e1cd90e
Martin A. López Espiña <martin@muylomas.com>
2026-01-26 17:11:13 -0300

    modified:   public/remate/lot-template.js

--- public/remate/lot-template.js
+++ public/remate/lot-template.js
@@ -3,7 +3,7 @@ const slideTemplate =
         <div id="lote-__lot_subId__" class="col-xl-3 col-lg-3 col-md-3 col-sm-6 col-12 bg-agrooriental stretch-card">
             <div class="card border-0 m-2" id="lot-__lot_lotId__" style="min-width: auto; min-width: -webkit-fill-available;">
                 <div id="cattle-media-container-__lot_lotId__" class="cattle-media-container position-relative overflow-hidden w-100">
-                    <img class="w-100" src="https://agro-oriental-remates.s3.us-west-1.amazonaws.com/web/images/banners/chimango-video-background.jpg">
+                    <img class="w-100" src="__lot_videoBackground__">
                     <div id="image-__lot_lotId__" 
                         class="cattle-image position-absolute top-0 start-0 w-100 h-100" 
                         style="background-image:url(__lot_imagesArray_0__);" 
