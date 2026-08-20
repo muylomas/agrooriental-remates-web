bae1ace71e762d7639e8c9d1e2d627af7dacaa0c
Martin A. López Espiña <martin@muylomas.com>
2026-08-19 22:03:18 -0300

    modified:   routes/criollosparaganar/index.js

--- routes/criollosparaganar/index.js
+++ routes/criollosparaganar/index.js
@@ -5,14 +5,9 @@ const common_auth = require('../common_auth');
 const auctions = require('./auctions');
 
 router.get('/', function (req, res, next) {
-    console.log("criollosparaganar 1");
     common_auth.basic(req.sessionID, function (reply) {
-        console.log("criollosparaganar 2");
         auctions.getViewParams(
             function (viewURLDir, indexParams, type) {
-
-                console.log("criollosparaganar 3");
-                console.log("type: " + type);
                 if (type == "redirect") {
                     res.redirect(viewURLDir);
                 }
