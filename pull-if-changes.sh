774581f33802221adc296c574329e207f05ed1ad
Martin A. López Espiña <martin@muylomas.com>
2026-08-25 17:37:05 -0300

    modified:   pull-if-changes.sh

--- pull-if-changes.sh
+++ pull-if-changes.sh
@@ -4,5 +4,6 @@ changed=0
 git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
 if [ $changed = 1 ]; then
     git pull
+    sudo runuser -l ubuntu -c 'cd '"$(pwd)"' && npm install --omit=dev'
     sudo runuser -l ubuntu -c 'NODE_ENV=production pm2 reload all'
 fi
